778430517e5088868ab42d47b8beedc451a5c6f5
Nick Verheijen <verheijen.webdevelopment@gmail.com>
2020-03-02 12:00:59 +0100

    Workstation switch

---
+++ src/Http/Requests/Messages/SendMessageRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tessify\Core\Http\Requests\Ministries;
+
+use Auth;
+use Illuminate\Foundation\Http\FormRequest;
+
+class SendMessageRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return Auth::check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            "user_id" => "required|exists:users,id",
+            "subject" => "nullable",
+            "message" => "required",
+        ];
+    }
+}
